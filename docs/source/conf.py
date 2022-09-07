0b35f602c9b9cc5fb25b5fe2c3c452e088553c78
erdem demirci <86264077+erdemirci@users.noreply.github.com>
2022-09-07 15:34:46 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,8 +3,8 @@
 # -- Project information
 
 project = 'MatLib'
-copyright = '2021, Graziella'
-author = 'Graziella'
+copyright = '2022, Erdem Demirci'
+author = 'Erdem Demirci'
 
 release = '0.1'
 version = '0.1.0'
